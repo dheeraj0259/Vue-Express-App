4d5f1e7782055ae87cd0a3d77aab9a1cd316f3c5
Dheeraj <dheeeraj0259@gmail.com>
2019-05-26 22:22:06 -0500

    -added loading icon

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <v-app>
-    <router-view />
+    <v-container grid-list-md>
+    <router-view/>
+    <div class="text-xs-center align-center">
+      <v-progress-circular indeterminate color="amber"></v-progress-circular>
+    </div>
+    </v-container>
   </v-app>
 </template>
 
+<script>
 export default {
   name: "app",
+  beforeMount: function () {
+console.log("first")
+  }
+};
+</script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 15rem;
 }
+</style>
