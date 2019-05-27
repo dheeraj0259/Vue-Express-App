e609d97f645fe0f1721dd3e65cb1c9953ea5d1f5
Dheeraj <dheeeraj0259@gmail.com>
2019-05-26 21:28:59 -0500

    -minor change

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="main-card">
-      <v-card class="mx-auto main-card" color="grey lighten-4" max-width="600">
-        <v-card-text
-          class="pt-4 font-weight-light grey--text title"
-          style="position: relative;"
-        >Application Name:</v-card-text>
-        <v-card-text
-          class="pt-4 display-1 font-weight-light orange--text text-xs-center"
-          style="position: relative;"
-        >Vue Express Tutorial</v-card-text>
-      </v-card>
+    <v-card class="mx-auto main-card" color="grey lighten-4" max-width="600">
+      <v-card-text class="pt-4 font-weight-light grey--text title">Application Name:</v-card-text>
+      <v-card-text
+        class="pt-4 display-1 font-weight-light orange--text text-xs-center"
+      >Vue Express Tutorial</v-card-text>
+    </v-card>
   </div>
 </template>
 
