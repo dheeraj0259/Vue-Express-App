6bbafbf574cda8153b548d2b4b27cf82a8e748de
Dheeraj <dheeeraj0259@gmail.com>
2019-06-01 13:52:22 -0500

    - customized table header

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,18 +1,34 @@
 <template>
-  <v-card class="theme--dark orange elevation-10">
     <v-data-table
       v-model="selected"
       :headers="headers"
       :items="tableItems"
+      :pagination.sync="pagination"
       select-all
       item-key="name"
       class="elevation-4"
     >
-      <template v-slot:headerCell="props">
-        <span
-          :class="'subheading font-weight-light text--darken-3 ' + headerColor + '--text'"
-          v-text="props.header.text"
-        />
+      <template v-slot:headers="props">
+        <tr>
+          <th>
+            <v-checkbox
+              :input-value="props.all"
+              :indeterminate="props.indeterminate"
+              primary
+              hide-details
+              @click.stop="toggleAll"
+            ></v-checkbox>
+          </th>
+          <th
+            v-for="header in props.headers"
+            :key="header.text"
+            :class="[pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'font-weight-bold' : 'font-weight-light', 'subheading text--darken-3 ' + headerColor + '--text']"
+            @click="changeSort(header.value)"
+          >
+            <v-icon small>arrow_upward</v-icon>
+            {{ header.text }}
+          </th>
+        </tr>
       </template>
 
       <template v-slot:items="props">
@@ -21,14 +37,24 @@
             <v-checkbox :input-value="props.selected" primary hide-details></v-checkbox>
           </td>
           <td
-            class="body-1 font-weight-light"
+            class="body-1 font-weight-light text-xs-center"
             v-for="header in headers"
             :key="header.name"
-          >{{ props.item[header.value] }}</td>
+          >
+          <span v-if="Array.isArray(props.item[header.value])">
+            <span v-for="(item, index) in props.item[header.value]" :key="index">
+            <v-chip color="#0f9fff" text-color="white" label>
+                  {{item}}
+            </v-chip>
+            </span>
+          </span>
+          <span v-else>
+          {{ props.item[header.value] }}
+          </span>
+          </td>
         </tr>
       </template>
     </v-data-table>
-  </v-card>
 </template>
 
 <script>
@@ -47,7 +73,23 @@ export default {
     }
   },
   data: () => ({
-    selected: []
-  })
+    selected: [],
+    pagination: {
+    }
+  }),
+  methods: {
+    toggleAll() {
+      if (this.selected.length) this.selected = [];
+      else this.selected = this.desserts.slice();
+    },
+    changeSort(column) {
+      if (this.pagination.sortBy === column) {
+        this.pagination.descending = !this.pagination.descending;
+      } else {
+        this.pagination.sortBy = column;
+        this.pagination.descending = false;
+      }
+    }
+  }
 };
 </script>
