153bbd93aa7e4af9cf5d9dc129252ac48dd96b17
Dheeraj <dheeeraj0259@gmail.com>
2019-09-29 21:56:24 -0500

    added multi text field logic

--- src/components/CloneStep1.vue
+++ src/components/CloneStep1.vue
@@ -7,23 +7,66 @@
     <v-card-title primary-title>
       <h3>Please choose your brands:</h3>
     </v-card-title>
-    <v-flex
-      xs3
-      d-flex
-      style="margin-left: 18px;"
-    >
-      <v-select
-        :items="items"
-        label="Select brands"
-        solo
-      />
-    </v-flex>
+    <v-container grid-list-md>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
+          xs3
+          d-flex
+          style="margin-left: 18px;"
+        >
+          <v-select
+            :items="items"
+            label="Select brands"
+            solo
+          />
+        </v-flex>
+        <v-flex
+          v-for="(textValue, index) in textValues"
+          :key="index"
+          xs3
+        >
+          <v-text-field
+            label="Minor Component"
+            :value="textValues[index].value"
+            :append-icon="getAppendIcon(index)"
+            @input="handleTextInput($event, index)"
+            @click:append="handleClickOnAppend(index)"
+          />
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-card>
 </template>
 
 <script>
 export default {
     name: "CloneStep1",
-    data: () => ({ items: ["DEKALB", "CHANNEL", "REGIONAL BRANDS", "CORN STATES"] }),
+    data: () => ({
+        items:      ["DEKALB", "CHANNEL", "REGIONAL BRANDS", "CORN STATES"],
+        textValues: [{ value: "5%: " }],
+    }),
+    methods: {
+        handleClickOnAppend: function (index) {
+            switch (this.getAppendIcon(index)) {
+                case "add":
+                    this.textValues.push({ value: "%: " });
+                    break;
+                case "delete":
+                    this.textValues = this.textValues.filter((item, itemIndex) => itemIndex !== index);
+                    break;
+                default:
+                    this.textValues.push({ value: "%: " });
+            }
+        },
+        handleTextInput: function (value, index) {
+            this.textValues[index].value = value;
+        },
+        getAppendIcon: function (index) {
+            return index + 1 === this.textValues.length ? "add" : "delete";
+        },
+    },
 };
 </script>
